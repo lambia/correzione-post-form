e85f8666f88739da6779c160fca2e0bb916259fb
Luca Lambia <luca@lambia.it>
2026-01-22 20:05:46 +0100

    state su singolo oggetto

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
 import { useState } from 'react'
 
+const postIniziale = {
+	author: "",
+	title: "",
+	body: "",
+	isPublic: true,
+};
+
 function App() {
-	const [author, setAuthor] = useState("");
-	const [title, setTitle] = useState("");
-	const [body, setBody] = useState("");
-	const [isPublic, setIsPublic] = useState(true);
 
-	function updateAuthor(e) {
+	const [post, setPost] = useState(postIniziale);
+
+	function updateProperty(e) {
 		const elemento = e.target;
 		const newValue = elemento.value;
-		setAuthor(newValue);
-	}
+		const key = elemento.name;
 
-	function updateTitle(e) {
-		const elemento = e.target;
-		const newValue = elemento.value;
-		setTitle(newValue);
-	}
-
-	function updateNody(e) {
-		const elemento = e.target;
-		const newValue = elemento.value;
-		setBody(newValue);
+		const nuovoPost = { ...post };
+		nuovoPost[key] = newValue;
+		setPost(nuovoPost);
 	}
 
 	function updateIsPublic(e) {
 		const elemento = e.target;
 		const newValue = elemento.checked;
-		setIsPublic(newValue);
-	}
 
+		const nuovoPost = { ...post };
+		nuovoPost.isPublic = newValue;
+		setPost(nuovoPost);
+	}
 
 	return (
 		<div>
 			<h1>hello react world</h1>
 			<ul>
-				<li>author: {author}</li>
-				<li>title: {title}</li>
-				<li>body: {body}</li>
-				<li>isPublic: {isPublic ? "si" : "no"}</li>
+				<li>author: {post.author}</li>
+				<li>title: {post.title}</li>
+				<li>body: {post.body}</li>
+				<li>isPublic: {post.isPublic ? "si" : "no"}</li>
 			</ul>
 			<form>
-				<input placeholder="author" value={author} onChange={updateAuthor} />
-				<input placeholder="title" value={title} onChange={updateTitle} />
-				<input placeholder="body" value={body} onChange={updateNody} />
-				<input type="checkbox" checked={isPublic} onChange={updateIsPublic} />
+				<input name="author" placeholder="author" value={post.author} onChange={updateProperty} />
+				<input name="title" placeholder="title" value={post.title} onChange={updateProperty} />
+				<input name="body" placeholder="body" value={post.body} onChange={updateProperty} />
+				<input name="isPublic" type="checkbox" checked={post.isPublic} onChange={updateIsPublic} />
 			</form>
 		</div>
 	)
